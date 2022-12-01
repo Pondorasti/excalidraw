30bb12720bc36f3d754cd5607f9596eb322ad67a
Alexandru Turcanu <pondorasti@gmail.com>
2022-11-30 17:41:38 -0800

    feat: set palette app version

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ExcalidrawApp from "./excalidraw-app";
 
 import "./excalidraw-app/pwa";
 import "./excalidraw-app/sentry";
+// @ts-ignore
+globalThis.PALETTE_APP_VERSION = process.env.VERCEL_GIT_COMMIT_SHA;
 window.__EXCALIDRAW_SHA__ = process.env.REACT_APP_GIT_SHA;
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
